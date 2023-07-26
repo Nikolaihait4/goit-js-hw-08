4496e92d8394973da1acbdb9bc8b5581aee5d472
Nikolaihait4 <jet1nikolai@gmail.com>
2023-07-26 10:53:33 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+const galleryItemUl = document.querySelector('.gallery');
+
+// Мэпаем массив записываем в переменную добавляем разметку
+const galleryEl = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  )
+  .join('');
+
+galleryItemUl.insertAdjacentHTML('beforeend', galleryEl);
+
+// Реализация библиотеки lightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  overlay: false,
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
